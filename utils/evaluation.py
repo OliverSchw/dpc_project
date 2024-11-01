2073a240ae4b6f286e71d551e66171b93f08670d
OliverSchw <oliver_schw@gmx.de>
2024-11-01 13:59:19 +0100

    pushed new experiments

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -16,9 +16,9 @@ from utils.signals import steps_ref_traj
 from utils.interactions import rollout_traj_env
 
 
-def steps_eval(env, reset_fun, policy, featurize, key, ref_len, step_lens=[50, 200]):
+def steps_eval(env, reset_fun, policy, featurize, key, ref_len, init_obs_key=None, step_lens=[50, 200]):
     obs_ref = steps_ref_traj(env, reset_fun, key, ref_len, step_lens=step_lens)
-    init_obs = reset_fun(env, None)
+    init_obs = reset_fun(env, init_obs_key)
     obs, acts = rollout_traj_env(policy, init_obs, obs_ref, ref_len, env, featurize)
     fig, axes = plt.subplots(2, 1, figsize=(20, 10), sharex=True)
     plot_i_dq_ref_tracking_time(obs, obs_ref, axes, tau=env.tau)
